f6f311f1087ca8885cc7155eacd72f31e1f0922a
trexloc <trexloc2004@gmail.com>
2025-02-24 11:27:53 +0700

    Lưu lại thay đổi trước khi pull

---
+++ build/web/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#eye').click(function() {
+        $(this).toggleClass('open'); // Toggle trạng thái mở
+        
+        // Sửa cách thay đổi icon
+        $(this).children('i').toggleClass('fa-eye-slash fa-eye'); 
+        
+        if ($(this).hasClass('open')) { // Dùng hasClass() thay vì hashClass()
+            $(this).prev('input').attr('type', 'text'); // Hiển thị mật khẩu
+        } else {
+            $(this).prev('input').attr('type', 'password'); // Ẩn lại mật khẩu
+        }
+    });
+});
